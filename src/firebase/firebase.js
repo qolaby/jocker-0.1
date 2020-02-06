4841b87da93b3230a8b08779b2aa31c4513bc10c
qolaby <labyad.qossai@gmail.com>
2020-02-06 21:33:41 +0100

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,14 +3,7 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCUOY-mdjKXYTI42VGPxLwrUZM6CMdkgys",
-  authDomain: "jocker-e3a24.firebaseapp.com",
-  databaseURL: "https://jocker-e3a24.firebaseio.com",
-  projectId: "jocker-e3a24",
-  storageBucket: "jocker-e3a24.appspot.com",
-  messagingSenderId: "204785758330",
-  appId: "1:204785758330:web:a9789dad0c05cf49878bc3",
-  measurementId: "G-186F9QRM2W"
+  Your firebase config
 };
 
 
